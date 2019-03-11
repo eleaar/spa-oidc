cbcc4098d999f5b42e77a64c9882d37a1914f27d
Daniel Krzywicki <daniel.krzywicki@fabernovel.com>
2019-03-11 18:04:38 +0100

    add simple routing and navbar

--- clients/implicit-flow/src/App.tsx
+++ clients/implicit-flow/src/App.tsx
@@ -1,27 +1,65 @@
-import * as React from 'react';
-import './App.css';
+import * as React from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom"
+
+import './App.css'
+import { NavBar } from './NavBar'
+
+function Index() {
+  return <h2>Home</h2>
+}
+
+function Login() {
+  return <h2>Login</h2>
+}
+
+function Logout() {
+  return <h2>Logout</h2>
+}
+
+function Secured() {
+  return <h2>Secured</h2>
+}
+
+function Hidden() {
+  return <h2>Hidden</h2>
+}
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <div>
+            <NavBar className='NavBar' links={[
+              {
+                target: "/", label: 'Home',
+              },
+              {
+                target: "/secured", label: 'Secured',
+              },
+              {
+                target: "/hidden", label: 'Hidden',
+              },
+              {
+                target: "/login", label: 'Login',
+              },
+              {
+                target: "/logout", label: 'Logout',
+              },
+            ]} />
+            <header className="App-header">
+              <Route path="/" exact component={Index} />
+              <Route path="/login/" component={Login} />
+              <Route path="/logout/" component={Logout} />
+              <Route path="/secured/" component={Secured} />
+              <Route path="/hidden/" component={Hidden} />
+            </header>
+          </div>
+        </Router>
       </div>
 
-    );
+    )
   }
 }
 
-export default App;
+export default App
